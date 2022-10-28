07686ee154f7225806323e7c2232ebbd3bfb2c7e
CR15T18N <94555829+CR15T18N@users.noreply.github.com>
2022-10-28 13:34:35 +0200

    prSeries

---
+++ prSeries/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import dao.SerieDao;
+import pojo.Serie;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Serie  serie = new Serie("Los Simpsons", 7, "Disney Plus");
+		SerieDao serieDao = new SerieDao();
+		serieDao.insert(serie);
+	}
+
+}
